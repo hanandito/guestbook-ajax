5d7202e2796237208d815d483095232cb037e1dd
Hanandito Delavega <hananditos@gmail.com>
2024-02-15 10:55:59 +0700

    init

--- comments.php
+++ comments.php
@@ -1,19 +1,43 @@
 <?php
 include_once("db_connect.php");
-if(!empty($_POST["name"]) && !empty($_POST["comment"])){
-	$insertComments = "INSERT INTO comment (parent_id, comment, sender) VALUES ('".$_POST["commentId"]."', '".$_POST["comment"]."', '".$_POST["name"]."')";
-	mysqli_query($conn, $insertComments) or die("database error: ". mysqli_error($conn));	
-	$message = '<label class="text-success my-5">Testimonial posted Successfully.</label>';
-	$status = array(
-		'error'  => 0,
-		'message' => $message
-	);	
-} else {
-	$message = '<label class="text-danger">Error: Testimonial not posted.</label>';
-	$status = array(
-		'error'  => 1,
-		'message' => $message
-	);	
-}
+$commentId = $_POST['commentId'];
+$comment = $_POST['comment'];
+$name = $_POST['name'];
+
+$rand = rand();
+$ekstensi =  array('png','jpg','jpeg','gif');
+$filename = $_FILES['foto']['name'];
+$ukuran = $_FILES['foto']['size'];
+$ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+
+
+if(!in_array($ext,$ekstensi) ) {
+	header("location:index.php?alert=gagal_ekstensi");
+   }else{
+	if($ukuran < 1044070){ 
+	$xx = $rand.'_'.$filename;
+	move_uploaded_file($_FILES['foto']['tmp_name'], 'gambar/'.$rand.'_'.$filename);
+	mysqli_query($koneksi, "INSERT INTO user VALUES(NULL,'$nama','$kontak','$alamat','$xx')");
+	header("location:index.php?alert=berhasil");
+	}else{
+	header("location:index.php?alert=gagak_ukuran");
+	}
+   }
+		if(!empty($name) && !empty($comment)){
+			$insertComments = "INSERT INTO comment (parent_id, comment, sender) VALUES ('$commentId', '$comment', '$name')";
+			mysqli_query($conn, $insertComments) or die("database error: ". mysqli_error($conn));	
+			$message = '<label class="text-success my-5">Testimonial posted Successfully.</label>';
+			$status = array(
+				'error'  => 0,
+				'message' => $message
+			);	
+		} else {
+			$message = '<label class="text-danger">Error: Testimonial not posted.</label>';
+			$status = array(
+				'error'  => 1,
+				'message' => $message
+			);	
+		}
 echo json_encode($status);
 ?>
